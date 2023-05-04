93ffac8d5bdb5b607946ec13814c9324faf837d8
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-05-04 08:55:07 +0600

    ***

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -8,6 +8,7 @@ export default function ProductForm({
   title: existingTitile,
   price: existingPrice,
   description: existingDescription,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitile || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -34,6 +35,26 @@ export default function ProductForm({
     router.push("/Products");
   }
 
+async function uploadImage(ev){
+   const files = ev.target?.files;
+   if(files?.length > 0){
+    const data = new FormData()
+    for (const file of files){
+        data.append('file',file);
+    }
+    
+    const res = await fetch ('/api/upload',{
+        method:'POST',
+        body:data,
+    })
+    console.log(res.data)
+   } 
+
+
+}
+
+
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product Name</label>
@@ -50,6 +71,19 @@ export default function ProductForm({
         value={price}
         onChange={(ev) => setPrice(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 border text-center flex flex-col rounded-lg bg-blue-200 items-center justify-center cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+</svg>
+Upload
+<input type="file" onChange={uploadImage} className="hidden" />
+</label>
+      </div>
+      {!images?.length && (
+        <div>No Photos found</div>
+      )}
       <label>Description</label>
       <textarea
         placeholder="description"
